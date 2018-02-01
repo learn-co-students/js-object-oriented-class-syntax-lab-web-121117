a831a16bc40ee6b81a4424e2faae88d673a7af55
github username <github email address>
2018-02-01 12:22:04 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+// class User {
+//   constructor(name, email) {
+//     this.name = name;
+//     this.email = email;
+//   }
+//
+//   sayHello() {
+//     console.log(`Hello, my name is ${this.name}`);
+//   }
+// }
+//
+//
+// class Teacher extends User {
+//   sayHello(){
+//     super.sayHello()
+//     console.log('hello')
+//   }
+// }
+
+
+class BoardMember{
+  constructor(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+  }
+  veto(){
+    return 'No, I must disagree'
+  }
+  approve(){
+    return 'You can do that!'
+  }
+  doCharity(){
+    return "I like to help people."
+  }
+
+  releasePressStatement(){
+    return "You will see great things from Scuber."
+  }
+
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+}
+
+class Ceo extends BoardMember{
+  constructor(name, homeState, training){
+  super(name, homeState, training)
+  }
+
+  hireEmployee(){
+    return 'Welcome aboard!'
+  }
+}
